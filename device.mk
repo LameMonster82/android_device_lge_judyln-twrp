f10bad54e1af9626bb8aea7a26d48132bc43cf8b
LameMonster82 <gregariivanov@gmail.com>
2020-06-02 17:36:57 +0200

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,18 +1,24 @@
 
 LOCAL_PATH := device/lge/judyln
 
-# define hardware platform
+# Define hardware platform
 PRODUCT_PLATFORM := sdm845
 
-#TEST
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)/bootctrl \
+    $(LOCAL_PATH)/kernel-headers
+
 # A/B support
 PRODUCT_PACKAGES += \
-    brillo_update_payload \
     otapreopt_script \
     cppreopts.sh \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload
+
 
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
@@ -20,7 +26,7 @@ PRODUCT_PACKAGES_DEBUG += \
 # Enable update engine sideloading by including the static version of the
 # boot_control HAL and its dependencies.
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sdm845 \
+    bootctrl.$(PRODUCT_PLATFORM) \
     libgptutils \
     libz \
     libcutils
@@ -31,7 +37,3 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
     hwservicemanager
-
-# cryptfs_hw can't be build using minimal-manifest twrp so using a prebuilt one
-#PRODUCT_COPY_FILES += \
-#    $(LOCAL_PATH)/recovery/root/sbin/libcryptfs_hw.so:vendor/lib64/libcryptfs_hw.so
